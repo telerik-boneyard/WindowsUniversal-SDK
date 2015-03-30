f81e5f9e9d66a01dcbe86d6e54156a4723bdc68b
TKonov <tsvyatco@gmail.com>
2015-03-30 11:23:49 +0300

    Added save chart to image example

---
+++ Chart/SaveToImage/ExportElementToImage.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+
+namespace ExportElementToImage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.NavigationCacheMode = NavigationCacheMode.Required;
+
+            this.radChart.DataContext = new double[] { 20, 30, 50, 10, 60, 40, 20, 80 };
+        }
+
+        /// <summary>
+        /// Invoked when this page is about to be displayed in a Frame.
+        /// </summary>
+        /// <param name="e">Event data that describes how this page was reached.
+        /// This parameter is typically used to configure the page.</param>
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            // TODO: Prepare page for display here.
+
+            // TODO: If your application contains multiple pages, ensure that you are
+            // handling the hardware Back button by registering for the
+            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
+            // If you are using the NavigationHelper provided by some templates,
+            // this event is handled for you.
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            StorageFolder picsFolder = KnownFolders.SavedPictures;
+            StorageFile file = await picsFolder.CreateFileAsync("savedChart.jpg", CreationCollisionOption.GenerateUniqueName);
+
+            await this.SaveVisualElementToFile(this.radChart, file);
+
+            var cd = new MessageDialog(string.Format("Image saved in Saved Pictures folder as {0}", file.Name));
+
+            await cd.ShowAsync();
+        }
+
+        private async Task SaveVisualElementToFile(FrameworkElement element, StorageFile file)
+        {
+            var renderTargetBitmap = new RenderTargetBitmap();
+            await renderTargetBitmap.RenderAsync(element, (int)element.ActualWidth, (int)element.ActualHeight);
+            var pixels = await renderTargetBitmap.GetPixelsAsync();
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await
+                    BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+                byte[] bytes = pixels.ToArray();
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+                                     BitmapAlphaMode.Ignore,
+                                     (uint)element.ActualWidth, (uint)element.ActualHeight,
+                                     96, 96, bytes);
+
+                await encoder.FlushAsync();
+            }
+        }
+    }
+}
---
+++ Chart/SaveToImage/ExportElementToImage/MainPage.xaml.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.Graphics.Imaging;
+using Windows.Storage;
+using Windows.Storage.Pickers;
+using Windows.Storage.Streams;
+using Windows.UI.Popups;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ExportElementToImage
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+
+            this.radChart.DataContext = new double[] { 20, 30, 50, 10, 60, 40, 20, 80 };
+        }
+
+        private async void Button_Click(object sender, RoutedEventArgs e)
+        {
+            FileSavePicker savePicker = new FileSavePicker();
+            savePicker.SuggestedFileName = "SavedImage.jpg";
+            savePicker.FileTypeChoices.Add("Image", new List<string>() { ".jpg" });
+
+            //Get the file the user selected
+            StorageFile saveFile = await savePicker.PickSaveFileAsync();
+
+            await this.SaveVisualElementToFile(this.radChart, saveFile);
+
+            var cd = new MessageDialog(string.Format("Image saved in Saved Pictures folder as {0}", saveFile.Name));
+
+            await cd.ShowAsync();
+        }
+
+        private async Task SaveVisualElementToFile(FrameworkElement element, StorageFile file)
+        {
+            var renderTargetBitmap = new RenderTargetBitmap();
+            await renderTargetBitmap.RenderAsync(element, (int)element.ActualWidth, (int)element.ActualHeight);
+            var pixels = await renderTargetBitmap.GetPixelsAsync();
+
+            using (IRandomAccessStream stream = await file.OpenAsync(FileAccessMode.ReadWrite))
+            {
+                var encoder = await
+                    BitmapEncoder.CreateAsync(BitmapEncoder.JpegEncoderId, stream);
+                byte[] bytes = pixels.ToArray();
+                encoder.SetPixelData(BitmapPixelFormat.Bgra8,
+                                     BitmapAlphaMode.Ignore,
+                                     (uint)element.ActualWidth, (uint)element.ActualHeight,
+                                     96, 96, bytes);
+
+                await encoder.FlushAsync();
+            }
+        }
+    }
+}
